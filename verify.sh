396e8ce6a942255dfd3c87e32d66406cbc6508da
pcf2025 <ab1995first@gmail.com>
2025-03-01 22:10:45 +0800

    Update verify.sh

--- verify.sh
+++ verify.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AQSTgQG_R_ckZPXLGW2kWCmcSuhK-G31m9T-gdGDQaBcNiAu1viq6o8YLwu4dptK-awAVQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AQSTgQHa9BNfAyma1Bl-kYrk88vRIUCLn3kR8t-9MauVX5yNXeUx9duWfgrnkCFZMkH6cw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
